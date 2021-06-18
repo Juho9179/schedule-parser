d88f234864577fc9a76fa2b0efd6a486b4e89c25
Juho9179 <Juho9179@gmail.com>
2021-06-18 18:40:05 +0300

    Row info implemented

--- parser.py
+++ parser.py
@@ -24,25 +24,38 @@
 #
 
 import openpyxl
-
+import openpyxl.utils as utils
 workbook = openpyxl.load_workbook("./test-material/ENSIH9_21(14.6-4.7).xlsx")
 
 
-# Returns True / False whether row is a dual-row or a single-row entry
-def is_dual(row):
-    # not implemented
-    return False
+# Returns row number for employee and checks whether dual row or single-row
+def get_row_info(employee_name):
+    row_info = {}
+    row_info["coordinate"] = ""
+    row_info["column"] = ""
+    row_info["row"] = 0
+    row_info["dual"] = False
 
-# Returns row number for employee
-def get_row(employee):
-    # not implemented
-    rows = workbook.active.iter_rows()
-    print(rows)
-    return 0
+    ws = workbook.active
+    for row in ws.iter_rows():
+        for cell in row:
+            # check if cell contents match employee name
+            if (cell.internal_value == employee_name):
+                row_info["coordinate"] = cell.coordinate
+                row_info["column"] = cell.column
+                row_info["row"] = cell.row
+                break
+                
+    # check whether row below is another employee or empty: singular or dual row
+    next_entry = ws.cell(row_info["row"] + 1, row_info["column"]).internal_value
+    if (next_entry == None):
+        row_info["dual"] = True
+
+    return row_info
 
 def main():
     # parse  n stuf
-    print(get_row("EH9"))
+    print(get_row_info("EH9"))
     return
 
 if __name__ == "__main__":
